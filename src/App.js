b43659fb109ab05b61ca34d75cc1b921db6379f0
Adam Czakon <adam.czakon@summer.agency>
2023-03-29 14:53:18 +0200

    Adjust teams config

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Configure from './Configure';
-import * as msTeams from '@microsoft/teams-js';
-import { useEffect, useState } from 'react';
+import TabConfig from './TabConfig';
 
 function App() {
-  const [isInitialized, setIsInitialized] = useState(false);
-
-  useEffect(() => {
-    msTeams.initialize(() => {
-      setIsInitialized(true);
-    });
-  }, []);
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-      {isInitialized && (
-        <BrowserRouter>
-          <Routes>
-            <Route path='/configure' element={<Configure />} />
-          </Routes>
-        </BrowserRouter>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div className='App'>
+              <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className='App-link'
+                  href='https://reactjs.org'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          }
+        />
+        <Route path='config' element={<TabConfig />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
